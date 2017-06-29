192e77cc47b4690925cc350aea5a027771ecd22c
Paula Ponce <paula.poncez@usach.cl>
2017-06-29 19:01:32 -0400

    botón remover

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,24 @@
 $(document).ready(function(){
-	var idCounter = 1; //contador para agregar id a cada checkbox, label y su p contenedor
-	
-	$("#tarea").click(function(){ //click para añadir tareas
-		var inputVal = $("#tarea").val();
-		$("#to-do").append("<p id='task-" + idCounter + "'><input type='checkbox' id='chk-" + idCounter + "'/><label class='black-text' for='chk-" + idCounter + "'>" + inputVal + "</label>" + "<a class='btn right-align' id='remove-btn-" + idCounter + "'>Remove</a></p>");
-		idCounter ++;
-	});
-	
-	$("#remove-btn-" + idCounter).click(function(event){ //click para remover tareas con el botón 'Remove'
-		event.preventDefault();
-		var parentId = $(this).parent("#task-" + idCounter);
-		$(parentId).remove();
-	});
-	
 
-	$(":checkbox").click(function(event){ //cambiar tareas completadas a sección completed-cont
+	$("#tarea").on("keyup", function(event){
 		event.preventDefault();
+		var idCounter = 1; //contador para agregar id a cada checkbox, label y su p contenedor
+		if(event.keyCode == 13){
+			var inputVal = $("#tarea").val(); //datos del input
+			
+			if(inputVal == ""){ //validación input
+				alert("Debes agregar una tarea");
+			}else{ //añade checkbox, label y button a sección To Do
+				$("#to-do").append("<p id='task-" + idCounter + "'><input type='checkbox' id='chk-" + idCounter + "'/><label class='black-text' for='chk-" + idCounter + "'>" + inputVal + "</label>" + "<a class='btn right-align remove-btn' id='remove-btn-" + idCounter + "'>Remove</a></p>");
+				$("#tarea").val(""); //limpia input
+				idCounter ++; //aumenta el contador de Id
+			}
+		}
+		$(".remove-btn").click(function(){ //click para remover tareas con el botón 'Remove'
 		var parentElement = $(this).parent("p");
-		$("#completed").append(parentElement);
+		$(parentElement).remove();
+		});
 	});
+	
 });
 
