076b90a5b6938ec8ff6a7ef2aa66662edb313eda
Paula <paula.poncez@usach.cl>
2017-06-29 10:58:43 -0400

    sección para tareas completadas

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,23 @@
 $(document).ready(function(){
 	var idCounter = 1; //contador para agregar id a cada checkbox, label y su p contenedor
+	
 	$("#tarea").click(function(){ //click para añadir tareas
 		var inputVal = $("#tarea").val();
 		$("#to-do").append("<p id='task-" + idCounter + "'><input type='checkbox' id='chk-" + idCounter + "'/><label class='black-text' for='chk-" + idCounter + "'>" + inputVal + "</label>" + "<a class='btn right-align' id='remove-btn-" + idCounter + "'>Remove</a></p>");
 		idCounter ++;
 	});
-	/*
-	$("button[id~='remove-btn']").click(function(){ //click para remover tareas
-		$(":checkbox").parent().remove();
+	
+	$("#remove-btn-" + idCounter).click(function(event){ //click para remover tareas con el botón 'Remove'
+		event.preventDefault();
+		var parentId = $(this).parent("#task-" + idCounter);
+		$(parentId).remove();
 	});
-	*/
+	
+
+	$(":checkbox").click(function(event){ //cambiar tareas completadas a sección completed-cont
+		event.preventDefault();
+		var parentElement = $(this).parent("p");
+		$("#completed").append(parentElement);
+	});
 });
+
